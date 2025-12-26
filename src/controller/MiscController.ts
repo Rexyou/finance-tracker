c9bb6c2d04f8f71b0d19a1e5152699c951ab72c4
Rex You <rexyou0831@gmail.com>
2025-12-26 14:10:20 +0800

    Adjust return response

--- src/controller/MiscController.ts
+++ src/controller/MiscController.ts
@@ -5,7 +5,7 @@ import { DashboardService } from "../services/DashboardService";
 export const getDashboardData = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const result = await DashboardService.getData(req.userData, req.body)
-        res.status(HttpCode.CREATED).json(result);
+        res.status(HttpCode.SUCCESS).json(result);
         return
     } catch (error) {
         next(error)
