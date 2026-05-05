8b70ad945e86cb9d2e0fdd06753dbda9530d6b5a
Rex You <rexyou0831@gmail.com>
2026-05-05 17:25:58 +0800

    Add time range

--- src/controller/MiscController.ts
+++ src/controller/MiscController.ts
@@ -1,10 +1,13 @@
 import type { NextFunction, Request, Response } from "express";
 import { HttpCode } from "../variables/errorCodes";
 import { DashboardService } from "../services/DashboardService";
+import { validateParameter } from "../utility/Validation";
+import { DatePaginationSchema } from "../variables/ValidationSchemas";
 
 export const getDashboardData = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await DashboardService.getData(req.userData, req.body)
+        const filterData = validateParameter(req, DatePaginationSchema)
+        const result = await DashboardService.getData(req.userData, filterData)
         res.status(HttpCode.SUCCESS).json(result);
         return
     } catch (error) {
